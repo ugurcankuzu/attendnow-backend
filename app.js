56c1d90685a23233e02de40fe092d5ce5cf243c1
ugurcankuzu <uci2001@hotmail.com>
2024-03-26 00:07:35 +0300

    Backend Cors Update

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const dotenv = require("dotenv");
 const connectDatabase = require("./Database/connectDatabase");
 const ErrorHandler = require("./Middleware/errors/ErrorHandler");
 const routers = require("./routers/main");
-
+const cors = require("cors");
 dotenv.config({
-
-    path : "./config/env/config.env"
+  path: "./config/env/config.env",
 });
 
 connectDatabase();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const PORT = process.env.PORT;
 
 app.use("/", routers);
@@ -20,6 +20,5 @@ app.use("/", routers);
 app.use(ErrorHandler);
 
 app.listen(PORT, () => {
-
-    console.log("Server Başlatıldı. PORT: ", PORT);
-})
+  console.log("Server Başlatıldı. PORT: ", PORT);
+});
