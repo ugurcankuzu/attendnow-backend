a5207876a097f04d330d92ceca28cc4bc8645be6
ugurcankuzu <uci2001@hotmail.com>
2024-03-26 11:34:05 +0300

    getLecturerById endpoint done

--- controllers/lecturerOps.js
+++ controllers/lecturerOps.js
@@ -1,5 +1,6 @@
 const asyncErrorWrapper = require("express-async-handler");
 const Session = require("../models/sessions");
+const Lecturer = require("../models/lecturer");
 const createSession = asyncErrorWrapper(async (req, res, next) => {
   const { id } = req.user;
   const { courseId } = req.query;
@@ -10,7 +11,25 @@ const createSession = asyncErrorWrapper(async (req, res, next) => {
     date: new Date().getUTCDate(),
     attendedStudents: [],
   });
-  res.status(200).json({ success: true, sessionId: session._id });
+  if (session) {
+    res.status(200).json({ success: true, sessionId: session._id });
+  } else {
+    next(new Error("Session oluştururken bir sorunla karşılaşıldı."));
+  }
+});
+const getLecturerById = asyncErrorWrapper(async (req, res, next) => {
+  const { id } = req.user;
+  const lecturer = await Lecturer.findById(id);
+  if (lecturer) {
+    res.status(200).json({
+      lecturerName: lecturer.name,
+      lecturerSurname: lecturer.surname,
+      lecturerCourses: lecturer.courses,
+      lecturerSessions: lecturer.sessions,
+    });
+  } else {
+    next(new Error("Eğitmen detaylarını alırken bir sorunla karşılaştık."));
+  }
 });
 
-module.exports = { createSession };
+module.exports = { createSession, getLecturerById };
